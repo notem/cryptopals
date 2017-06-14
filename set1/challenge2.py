e268c6f48d083b90cb4f86d5e399a7e229b83bea
notem <njm3308@rit.edu>
2017-06-14 13:02:42 -0400

    Completed challenge2

---
+++ set1/challenge2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# author: Nate Mathews, njm3308@rit.edu
+# date: 2017-06-14
+
+import sys
+from sys import argv
+import binascii
+
+
+def xor(string1, string2):
+    # convert to byte arrays
+    binary1 = bytearray(binascii.unhexlify(string1))
+    binary2 = bytearray(binascii.unhexlify(string2))
+    # xor each byte
+    for i in range(len(binary1)):
+        binary1[i] ^= binary2[i]
+    # convert back to a hex string
+    return binascii.hexlify(binary1)
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 2:
+        len1 = len(argv[1])
+        len2 = len(argv[2])
+        if len1 == len2:
+            hex_str = xor(argv[1], argv[2])
+            print(hex_str)
+        else:
+            print("Error: hex strings must of same length!")
+    else:
+        print("Usage: prog [hex_string1] [hex_string2]\nOutput: Base64 encoded string")
