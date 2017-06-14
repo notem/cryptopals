b4e45a609f7c51bc1f2de40df1a4ecf4fa97ff38
notem <njm3308@rit.edu>
2017-06-14 14:59:08 -0400

    Wrote a basic solution for challenge3

---
+++ set1/challenge3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# author: Nate Mathews, njm3308@rit.edu
+# date: 2017-06-14
+
+import sys
+from sys import argv
+import binascii
+
+
+def xor(string, byte):
+    binary = bytearray(binascii.unhexlify(string))
+    for i in range(len(binary)):
+        binary[i] ^= byte
+    try:
+        return binary.decode()
+    except UnicodeDecodeError:
+        return "Error"
+
+if __name__ == '__main__':
+    if len(argv[1:]) == 1:
+        for i in range(0,255):
+            hex_str = xor(argv[1], i)
+            print(hex_str, "\t|\tbyte:", i)
+    else:
+        print("Usage: prog [hex_string]\nOutput: list of all results of xor'ing the hex encoded string with a single repeating byte")
